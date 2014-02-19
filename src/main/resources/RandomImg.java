317d1cd282a24450ccf895aa435c2dcf4b045c9c
free <li5220008@163.com>
2014-02-19 18:24:31 +0800

    img

---
+++ src/main/resources/RandomImg.java
@@ -0,0 +1,10 @@
+import javax.servlet.http.HttpServlet;
+/**
+ * Desc:
+ * User: weiguili(li5220008@gmail.com)
+ * Date: 14-2-19
+ * Time: 下午5:54
+ */
+public class RandomImg extends HttpServlet {
+
+}

086d8a1ddb92b41f1ca7dcfe723afe6bb6513977
free <li5220008@163.com>
2014-02-21 17:22:34 +0800

    forward

--- src/main/resources/request/RequestForm.java
+++ src/main/resources/request/RequestForm.java
@@ -12,5 +12,30 @@ import java.io.IOException;
 public class RequestForm extends HttpServlet {
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        test(req);
+
+    }
+    //解决POST乱码
+    private void test(HttpServletRequest req) {
+        //req.setCharacterEncoding("UTF-8");//只对POST方法有效
+        System.out.println(req.getParameter("username"));
+        System.out.println(req.getParameter("password"));
+        System.out.println(req.getParameter("checkcode"));
+        System.out.println(req.getParameter("gender"));
+        System.out.println(req.getParameter("address"));
+        String[] likes = req.getParameterValues("likes");
+        for(int i=0;likes !=null&&i<likes.length;i++){
+            System.out.println(likes[i]);
+        }
+        System.out.println(req.getParameter("description"));
+        System.out.println(req.getParameter("hidden"));
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String username = req.getParameter("username");
+        String encodeName = new String(username.getBytes("iso8859-1"),"UTF-8");
+        System.out.println(encodeName);
+        doPost(req, resp);
     }
 }
